0e0c0c7e0cb3f4ffd80c9ca497173d3c3f327e4e
rurulee <push511007@gmail.com>
2019-09-11 18:23:16 +0800

    start

--- src/js/all.js
+++ src/js/all.js
@@ -1,40 +1,60 @@
-import luxy from 'luxy.js';
-import baguetteBox from 'baguettebox.js';
-import bsn from 'bootstrap.native/dist/bootstrap-native-v4';
-
-//luxy
-var w = window.innerWidth ||
-    document.documentElement.clientWidth ||
-    document.body.clientWidth;
-if (w > 800) {
-    luxy.init({
-        wrapper: '#luxy',
-        targets: '.luxy-el',
-        wrapperSpeed: 0.08
-    });
-
-} else {
-    luxy.uninit()
-};
-
-// Lightbox 
-baguetteBox.run('.baguetteBox-gallery');
+import Swiper from 'swiper';
 
 
-/* 按下GoTop按鈕時的事件 */
-$(function() {
-    /* 按下GoTop按鈕時的事件 */
-    $('#gotop').click(function() {
-        $('html,body').animate({ scrollTop: 0 }, 'slow'); /* 返回到最頂上 */
-        return false;
-    });
-
-    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 400) {
-            $('#gotop').fadeIn(300);
-        } else {
-            $('#gotop').fadeOut();
-        }
-    });
+const banner = new Swiper('.swiper-container-1', {
+    navigation: {
+        nextEl: '.swiper-button-next-1',
+        prevEl: '.swiper-button-prev-1',
+    },
+    pagination: {
+        el: '.swiper-pagination-1',
+    },
+    autoplay: {
+        delay: 3000,
+    },
 });
+
+const chapterRow = new Swiper('.chapter-row__swiper', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    slidesPerGroup: 5,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.swiper-button-next-2',
+        prevEl: '.swiper-button-prev-2',
+    }
+});
+
+
+Vue.component("item-list", {
+        template: '<div class="nav__active"><a v-for="item in items" :href="item.url" class="header__link">{{ item.item }}</a></div>',
+        data: function() {
+            return {
+                items: [{
+                    item: "about",
+                    url: "index.html"
+                }, {
+                    item: "web",
+                    url: "#"
+                }, {
+                    item: "design",
+                    url: "#"
+                }]
+            }
+        }
+    }),
+    new Vue({
+        el: ".menu__nav"
+    });
+
+
+// for (var ae = window.location.href, ne = $(".nav__active a"), re = 0; re < ne.length; re++) {
+//     var le = ne.eq(re).prop("href"); -
+//     1 != ae.indexOf(le) && ne.eq(re).addClass("header__link--active")
+// }
+// "http://ctbc.develop.wakool.net/" != location.href && "http://ctbc.develop.wakool.net/#" != location.href || ne.eq(0).addClass("header__link--active"),
+//     $(".showmenu").on("click", function(e) {
+//         e.preventDefault(),
+//             $("body").toggleClass("hamburger-expanded")
+//     })
