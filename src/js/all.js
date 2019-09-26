561826a1966cc243e727d59ab97ecc528d2ba6dd
rurulee <push511007@gmail.com>
2019-09-26 18:38:18 +0800

    改fontSize

--- src/js/all.js
+++ src/js/all.js
@@ -27,26 +27,26 @@ const chapterRow = new Swiper('.chapter-row__swiper', {
 });
 
 
-Vue.component("item-list", {
-        template: '<div class="nav__active"><a v-for="item in items" :href="item.url" class="header__link">{{ item.item }}</a></div>',
-        data: function() {
-            return {
-                items: [{
-                    item: "about",
-                    url: "index.html"
-                }, {
-                    item: "web",
-                    url: "#"
-                }, {
-                    item: "design",
-                    url: "#"
-                }]
-            }
-        }
-    }),
-    new Vue({
-        el: ".menu__nav"
-    });
+// Vue.component("item-list", {
+//         template: '<div class="nav__active"><a v-for="item in items" :href="item.url" class="header__link">{{ item.item }}</a></div>',
+//         data: function() {
+//             return {
+//                 items: [{
+//                     item: "about",
+//                     url: "index.html"
+//                 }, {
+//                     item: "web",
+//                     url: "#"
+//                 }, {
+//                     item: "design",
+//                     url: "#"
+//                 }]
+//             }
+//         }
+//     }),
+//     new Vue({
+//         el: ".menu__nav"
+//     });
 
 
 // for (var ae = window.location.href, ne = $(".nav__active a"), re = 0; re < ne.length; re++) {
