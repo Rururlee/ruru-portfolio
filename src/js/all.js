fdd8c5d4902c3e57485b43ef5f4cbbcb979b93fe
rurulee <push511007@gmail.com>
2019-10-16 18:42:21 +0800

    add about

--- src/js/all.js
+++ src/js/all.js
@@ -1,16 +1,10 @@
 $(window).scroll(function() {
     var scrollTop = $(window).scrollTop();
     var height = $(window).height();
-    $('#fadeout').css({
+    $('.fadeout').css({
         'opacity': ((height - scrollTop) / height),
         '-khtml-opacity': ((height - scrollTop) / height)
     });
 });
 
 
-// $('.header__nav a:nth-child(2)').click(function() {
-//     $('html,body').animate({ scrollTop: $('#work').offset().top + 35 }, 500);
-// });
-// $('#goto').click(function() {
-//     $('html,body').animate({ scrollTop: $('#work').offset().top + 35 }, 500);
-// });
