62c2d5f5fc074cff3f7f7f6b0c10e11183a6dc86
Rururlee <push511007@gmail.com>
2020-05-24 15:20:40 +0800

    me

--- src/js/all.js
+++ src/js/all.js
@@ -1,10 +1,8 @@
-$(window).scroll(function() {
-    var scrollTop = $(window).scrollTop();
-    var height = $(window).height();
-    $('.fadeout').css({
-        'opacity': ((height - scrollTop) / height),
-        '-khtml-opacity': ((height - scrollTop) / height)
-    });
-});
-
-
+// $(window).scroll(function() {
+//     var scrollTop = $(window).scrollTop();
+//     var height = $(window).height();
+//     $('.fadeout').css({
+//         'opacity': ((height - scrollTop) / height),
+//         '-khtml-opacity': ((height - scrollTop) / height)
+//     });
+// });
