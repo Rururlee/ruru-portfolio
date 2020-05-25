69009ed24c4d122ad978a915830d152efaf9a716
rurulee <push511007@gmail.com>
2020-05-25 18:05:59 +0800

    menu

--- src/js/all.js
+++ src/js/all.js
@@ -1,8 +1,43 @@
-// $(window).scroll(function() {
-//     var scrollTop = $(window).scrollTop();
-//     var height = $(window).height();
-//     $('.fadeout').css({
-//         'opacity': ((height - scrollTop) / height),
-//         '-khtml-opacity': ((height - scrollTop) / height)
-//     });
-// });
+//mobile menu
+
+$('.nav-link:nth-child(1)').click(function () {
+  $('html,body').animate({ scrollTop: $('.work__title').offset().top - 50 }, 500)
+})
+$('.nav-link:nth-child(2)').click(function () {
+  $('html,body').animate({ scrollTop: $('.about__title').offset().top - 50 }, 500)
+})
+
+$('.nav-link:nth-child(3)').click(function () {
+  $('html,body').animate({ scrollTop: $('.contact__title').offset().top }, 500)
+})
+
+//content-btn
+$('.content-btn').click(function () {
+  $('html,body').animate({ scrollTop: $('.contact').offset().top }, 500)
+})
+
+window.addEventListener('load', function (event) {
+  setTimeout(function () {
+    $('.loading').fadeOut(1000)
+  }, 500)
+})
+
+//平面設計lightbox
+$('.work__img--show').click(function () {
+  //console.log(this.id)
+  let imgUrl = 'img/' + this.id + '_2.png'
+  //console.log(imgUrl)
+  $('.show__work').fadeIn('fast')
+  $('.show__work--img').attr('src', imgUrl)
+})
+
+$('.show__work').click(function () {
+  $('.show__work').fadeOut('fast')
+  $('.show__work--img').attr('src', '')
+  // $('body').removeClass('overflow-hidden')
+})
+$('.header').click(function () {
+  $('.show__work').fadeOut('fast')
+  $('.show__work--img').attr('src', '')
+  // $('body').removeClass('overflow-hidden')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = (env) => {
       ],
     },
     devServer: {
-      contentBase: './',
+      contentBase: './js',
       hot: true,
     },
   }
